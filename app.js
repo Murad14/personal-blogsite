6b0c950e7f5c939749b8c88fcf33e551d3c65165
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-23 02:37:20 +0600

    modified

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000
-express.json() //body-parser
+const port = 3000;
+const bodyParser = require("body-parser");
+app.use(express.urlencoded({ extended: true }));
 const ejs = require("ejs");  //ejs
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -26,6 +27,14 @@ app.get('/contact', (req, res) => {
   res.render('contact', {contactContent});
 })
 
+app.get('/compose', (req, res) => {
+  res.render('compose', {});
+})
+
+app.post('/compose', (req, res) => {
+  console.log(req.body.inputText);
+  res.redirect('/');
+})
 
 
 
