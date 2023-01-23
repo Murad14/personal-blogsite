d0fa254c1b7902196542912e6afd21e2f0471665
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-23 21:36:20 +0600

    modified

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.post('/compose', (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
