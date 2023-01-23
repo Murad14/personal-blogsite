85d5dc6475c1beb7687df8ad1451ff08259ffe44
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-24 00:53:09 +0600

    lodash implemented

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 const bodyParser = require("body-parser");
 app.use(express.urlencoded({ extended: true }));
 const ejs = require("ejs");  //ejs
+const _ = require('lodash'); //lodash
 global.POSTS = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -43,13 +44,15 @@ app.post('/compose', (req, res) => {
 
 // Route Parameters
 app.get('/posts/:postName', (req, res) => {
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName); //lowercase implementation(khabab case)
 
   POSTS.forEach(post => {
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
-      console.log("Match");
+      console.log("Match Found!");
+  }else{
+    console.log('Not found!');
   }
   });
 
